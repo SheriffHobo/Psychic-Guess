934e3e4979f1ffd9cd92983cc436c8e769ac7f03
SheriffHobo <zombiemaster@gmail.com>
2019-02-03 18:55:41 -0800

    First bunch of updates across all pages

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,36 @@
+const compChoices = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  
+let wins = 0;
+let losses = 0;
+let chances = 20;
+let guesses = [];
+
+document.onkeyup = function(event) {
+    let userGuess = event.key;
+    let compGuess = compChoices[Math.floor(Math.random() * compChoices.length)];
+if (compGuess === userGuess) {
+    wins++;
+    guesses.push(userGuess);
+    chances--;
+    alert('You did it! Press ok and keep going!');
+} else if (compGuess !== userGuess) {
+    losses++;
+    guesses.push(userGuess);
+    chances--;
+    }
+if (chances === 0) {
+    alert("Sorry, you've run out of chances. Try again?");
+    location.reload(); // Reloads the document
+} else if (wins === 5) {
+    alert("Call Vegas! We have a real psyhcic here!");
+    location.reload();
+    }
+
+    document.getElementById("picks1").innerHTML = userGuess
+    document.getElementById("picks2").innerHTML = guesses
+    document.getElementById("chanceleft").innerHTML = chances
+    document.getElementById("wincounter").innerHTML = wins
+    document.getElementById("losscounter").innerHTML = losses
+
+};
+
